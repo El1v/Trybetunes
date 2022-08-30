23b35b3f608ba15009e00956c8330ecc9901e045
Elivelton <eliveltonmn@outlook.com>
2022-08-30 16:35:11 -0300

    Arruma lint

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
